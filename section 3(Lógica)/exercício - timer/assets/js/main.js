4abd394cf780bb7de081c7335305e7f7c113eac2
Andrei <andreihenrique1607@gmail.com>
2026-06-18 17:22:08 -0300

    timer

---
+++ section 3(Lógica)/exercício - timer/assets/js/main.js
@@ -0,0 +1,63 @@
+function meuRelogio() {
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+
+    console.log(criaHoraDosSegundos(10));
+
+    const relogio = document.querySelector('.relogio');
+    // const iniciar = document.querySelector('.iniciar');
+    // const pausar = document.querySelector('.pausar');
+    // const zerar = document.querySelector('.zerar');
+    let segundos = 0;
+    let timer;
+
+    function iniciaRelogio() {
+        timer = setInterval(function () {
+            segundos++;
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000);
+    };
+
+    // iniciar.addEventListener('click', function(event){
+    //     relogio.classList.remove('pausado');
+    //     clearInterval(timer);
+    //     iniciaRelogio();
+    // });
+    // pausar.addEventListener('click', function(event){
+    //     relogio.classList.add('pausado');
+    //     clearInterval(timer);
+    // });
+    // zerar.addEventListener('click', function(event){
+    //     clearInterval(timer);
+    //     relogio.innerHTML = '00:00:00';
+    //     segundos = 0;
+    // });
+
+    document.addEventListener('click', function (e) {
+        const el = e.target;
+
+        if (el.classList.contains('zerar')) {
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            segundos = 0;
+            relogio.classList.remove('pausado');
+        }
+
+        if (el.classList.contains('iniciar')) {
+            clearInterval(timer);
+            relogio.classList.remove('pausado');
+            iniciaRelogio();
+        }
+
+        if (el.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('pausado')
+        }
+    })
+}
+meuRelogio();
