a50e34e9ca10c6e70f4f580ccea10a3013cd35d8
Andrei <andreihenrique1607@gmail.com>
2026-07-07 13:55:27 -0300

    session

--- section 10 (NodeJS)/session/server.js
+++ section 10 (NodeJS)/session/server.js
@@ -13,9 +13,26 @@ mongoose.connect(process.env.CONNECTIONSTRING)
     })
     .catch(e => console.log(e));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: 'fegosdjaohduisf feanofksn',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+})
+app.use(sessionOptions);
+app.use(flash());
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
