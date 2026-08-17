e8ab36fe9e66dfc10083486f30faf843f791ff31
Andrei Henrique <andreihenrique1607@gmail.com>
2026-08-16 21:54:51 -0300

    token

--- section_13(api_rest)/api_rest/src/routes/userRoutes.js
+++ section_13(api_rest)/api_rest/src/routes/userRoutes.js
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
+//Não deveria existir em uma aplicação real
+router.get('/:id', userController.show);
+router.get('/', loginRequired, userController.index);
+
 router.post('/', userController.store);
+router.put('/', loginRequired, userController.update);
+router.delete('/', loginRequired, userController.delete);
 
 export default router;
